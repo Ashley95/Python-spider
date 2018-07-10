81535dd09dedd301531636021f778bd90d3eaf50
Ashley95 <38779903+Ashley95@users.noreply.github.com>
2018-07-10 16:39:34 +0800

    Delete 123.py

--- mafengwo/123.py
+++
@@ -1,8 +0,0 @@
-def main():
-    ab = 'http://www.mafengwo.cn/travel-scenic-spot/mafengwo/10065.html'
-    a = [i for i in ab.split('/')][-1]
-    b = a.split('.')[0]
-    print(b)
-
-if __name__ == '__main__':
-    main()
